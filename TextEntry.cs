919c721736d8538d7131687642bc640facad55b3
Star Rauchenberger <fefferburbia@gmail.com>
2024-03-19 14:07:02 -0400

    Added caret to text field

--- TextEntry.cs
+++ TextEntry.cs
@@ -7,6 +7,72 @@ using UnityEngine;
 namespace TaijiRandomizer
 {
     [RegisterTypeInIl2Cpp]
+    class Caret : MonoBehaviour
+    {
+        public Caret(System.IntPtr ptr) : base(ptr) { }
+
+        private TextEntry? _textEntry = null;
+        private TextMeshPro? _textMeshPro = null;
+        private SpriteRenderer? _sprite = null;
+
+        void Start()
+        {
+            GameObject parentObject = gameObject.transform.parent.gameObject;
+            _textEntry = parentObject.GetComponent<TextEntry>();
+            _textMeshPro = parentObject.GetComponent<TextMeshPro>();
+
+            _sprite = gameObject.AddComponent<SpriteRenderer>();
+            _sprite.sprite = GameObject.Find("New Pause Menu").GetComponent<PauseMenu>().sliderPrefab.GetComponent<SpriteRenderer>().sprite;
+            _sprite.sortingLayerName = "UI/Cursor";
+            _sprite.sortingOrder = 4;
+
+            Bounds bounds = new();
+            bounds.center = new(0, 0, 0);
+            bounds.extents = new(0.25F, 1);
+            _sprite.bounds = bounds;
+
+            transform.set_localScale_Injected(new(0.01F, 0.45F, 1));
+        }
+
+        void Update()
+        {
+            if (_textEntry.InputActivated)
+            {
+                _sprite.enabled = true;
+
+                if (_textEntry.Position <= _textMeshPro.textInfo.characterCount)
+                {
+                    float x = 0;
+                    float y = 0;
+                    TMP_LineInfo lineInfo;
+
+                    if (_textEntry.Position > 0)
+                    {
+                        TMP_CharacterInfo charInfo = _textMeshPro.textInfo.characterInfo[_textEntry.Position - 1];
+                        lineInfo = _textMeshPro.textInfo.lineInfo[charInfo.lineNumber];
+                        x = charInfo.topRight.x + 0.05F;
+                    }
+                    else
+                    {
+                        TMP_CharacterInfo charInfo = _textMeshPro.textInfo.characterInfo[0];
+                        lineInfo = _textMeshPro.textInfo.lineInfo[charInfo.lineNumber];
+
+                        x = charInfo.topLeft.x - 0.05F;
+                    }
+
+                    y = lineInfo.baseline + 0.2F;
+
+                    gameObject.transform.set_localPosition_Injected(new(x, y, 0));
+                }
+            }
+            else
+            {
+                _sprite.enabled = false;
+            }
+        }
+    }
+
+    [RegisterTypeInIl2Cpp]
     class TextEntry : MonoBehaviour
     {
         public delegate void InputFinishedHandler(bool success);
@@ -16,6 +82,7 @@ namespace TaijiRandomizer
 
         private InputFinishedHandler? _inputFinishedHandler = null;
         private PauseMenu.MenuItem? _menuItem = null;
+        private Caret? _caret = null;
 
         private bool _inputActivated = false;
         private bool _firstFrame = false;
@@ -23,6 +90,16 @@ namespace TaijiRandomizer
 
         private string _text = "";
         private int _position = 0;
+
+        public bool InputActivated
+        {
+            get { return _inputActivated; }
+        }
+
+        public int Position
+        {
+            get { return _position; }
+        }
 
         public string Text
         {
@@ -72,6 +149,15 @@ namespace TaijiRandomizer
             _position = _text.Length;
 
             Keyboard.current.add_onTextInput(new System.Action<char>(ProcessInput));
+
+            if (_caret == null)
+            {
+                GameObject caretObject = new("Caret");
+                caretObject.transform.parent = gameObject.transform;
+                caretObject.layer = LayerMask.NameToLayer("OffScreen_Buffer");
+
+                _caret = caretObject.AddComponent<Caret>();
+            }
         }
 
         public void ActivateInputField()
@@ -165,6 +251,22 @@ namespace TaijiRandomizer
                 {
                     SetText(_text.Insert(_position, GUIUtility.systemCopyBuffer));
                     _position += GUIUtility.systemCopyBuffer.Length;
+                }
+
+                if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+                {
+                    if (_position > 0)
+                    {
+                        _position--;
+                    }
+                }
+
+                if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+                {
+                    if (_position < _text.Length)
+                    {
+                        _position++;
+                    }
                 }
             }
         }
