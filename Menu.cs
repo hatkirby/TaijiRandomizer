95a6d1c3709d5ca0d9499800d864968f162759cc
Star Rauchenberger <fefferburbia@gmail.com>
2024-03-19 11:28:00 -0400

    Straight up just implementing a text field

---
+++ Menu.cs
@@ -0,0 +1,237 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppInterop.Runtime;
+using Il2CppTMPro;
+using UnityEngine;
+
+namespace TaijiRandomizer
+{
+    [HarmonyPatch(typeof(PauseMenu), "InitializeMenus")]
+    static class Menu
+    {
+        public static PauseMenu? pauseMenu = null;
+        public static PauseMenu.SubMenu randomizerMenu;
+
+        private static PauseMenu.MenuItem? _seedMenuItem = null;
+
+        private static bool _setSeed = false;
+        private static int _setSeedValue = 1000000;
+
+        public static bool SetSeed
+        {
+            get { return _setSeed; }
+
+            set
+            {
+                _setSeed = value;
+
+                if (_seedMenuItem != null)
+                {
+                    if (_setSeed)
+                    {
+                        _seedMenuItem.hidden = false;
+                        _seedMenuItem.obj.active = true;
+                    }
+                    else
+                    {
+                        _seedMenuItem.hidden = true;
+                        _seedMenuItem.obj.active = false;
+                    }
+                }
+            }
+        }
+
+        public static int SetSeedValue
+        {
+            get { return _setSeedValue; }
+        }
+
+        public delegate int GetChoiceFunction();
+        public delegate void SetChoiceFunction(int choice);
+
+        public delegate string GetStringFunction();
+        public delegate void SetStringFunction(string value);
+
+        public static PauseMenu.MenuItem CreateMenuItem(string text)
+        {
+            GameObject menuDisableGroup = pauseMenu.menuDisableGroup;
+            Transform transform = menuDisableGroup.transform;
+
+            GameObject menuObject = GameObject.Instantiate(pauseMenu.optionPrefab);
+            menuObject.transform.parent = transform;
+
+            PauseMenu.MenuItem menuItem = new()
+            {
+                obj = menuObject,
+                locString = "MENU_OFF",
+                type = PauseMenu.WidgetType.subMenu,
+                text = menuObject.GetComponent<TextMeshPro>(),
+                func = null,
+                belowMenu = null,
+                hidden = false
+            };
+
+            menuItem.text.m_HorizontalAlignment = HorizontalAlignmentOptions.Right;
+            menuItem.text.m_VerticalAlignment = VerticalAlignmentOptions.Middle;
+            menuItem.text.m_havePropertiesChanged = true;
+            menuItem.text.SetVerticesDirty();
+            menuItem.text.SetText(text);
+            menuItem.text.color = Constants.WHITECLEAR_COLOR;
+
+            return menuItem;
+        }
+
+        public static PauseMenu.MenuItem CreateActionMenuItem(string text, System.Action action)
+        {
+            PauseMenu.MenuItem menuItem = CreateMenuItem(text);
+            menuItem.func = DelegateSupport.ConvertDelegate<PauseMenu.menuFunctionDelegate>(action);
+
+            return menuItem;
+        }
+
+        public static PauseMenu.MenuItem CreateSubMenuItem(string text, PauseMenu.SubMenu subMenu)
+        {
+            PauseMenu.MenuItem menuItem = CreateMenuItem(text);
+            menuItem.belowMenu = subMenu;
+            menuItem.func = DelegateSupport.ConvertDelegate<PauseMenu.menuFunctionDelegate>(pauseMenu.GoToBelowMenu);
+
+            return menuItem;
+        }
+
+        public static PauseMenu.MenuItem CreateReturnMenuItem()
+        {
+            PauseMenu.MenuItem menuItem = CreateMenuItem("<<");
+            menuItem.func = DelegateSupport.ConvertDelegate<PauseMenu.menuFunctionDelegate>(pauseMenu.GoToAboveMenu);
+
+            return menuItem;
+        }
+
+        public static PauseMenu.MenuItem CreateChoiceMenuItem(string text, List<string> choices, GetChoiceFunction getChoiceFunc, SetChoiceFunction setChoiceFunc)
+        {
+            PauseMenu.SubMenu subMenu = new()
+            {
+                isBottomMenu = true,
+                depth = 1,
+                activeItem = getChoiceFunc()
+            };
+
+            foreach (string choice in choices)
+            {
+                subMenu.items.Add(CreateActionMenuItem(choice, () =>
+                {
+                    setChoiceFunc(subMenu.activeItem);
+                    subMenu.activeItem = getChoiceFunc();
+                    pauseMenu.GoToAboveMenu();
+                }));
+            }
+
+            pauseMenu.menus.Add(subMenu);
+
+            PauseMenu.MenuItem menuItem = CreateSubMenuItem(text, subMenu);
+            menuItem.type = PauseMenu.WidgetType.toggler;
+            menuItem.currentSettingFunc = DelegateSupport.ConvertDelegate<PauseMenu.getCurrentSettingDelegate>(getChoiceFunc);
+
+            return menuItem;
+        }
+
+        public static PauseMenu.MenuItem CreateTextEntryMenuItem(string text, GetStringFunction getStringFunc, SetStringFunction setStringFunc)
+        {
+            GameObject textEntry = GameObject.Instantiate(pauseMenu.optionPrefab);
+            textEntry.transform.parent = pauseMenu.menuDisableGroup.transform;
+
+            TextMeshPro textObject = textEntry.GetComponent<TextMeshPro>();
+            textObject.horizontalAlignment = HorizontalAlignmentOptions.Left;
+            textObject.enableWordWrapping = true;
+
+            RectTransform textTransform = textEntry.GetComponent<RectTransform>();
+            textTransform.offsetMin = new(0.5F, -0.75F);
+            textTransform.offsetMax = new(8, 0.3422F);
+            textTransform.pivot = new(0.04F, 0.46F);
+
+            PauseMenu.MenuItem subMenuItem = new()
+            {
+                obj = textEntry,
+                locString = "MENU_OFF",
+                type = PauseMenu.WidgetType.subMenu,
+                text = textObject,
+                func = null,
+                belowMenu = null,
+                hidden = false
+            };
+
+            TextEntry menuInputHandler = textEntry.AddComponent<TextEntry>();
+            menuInputHandler.MenuItem = subMenuItem;
+            menuInputHandler.Text = getStringFunc();
+            menuInputHandler.OnInputFinished = (bool success) =>
+            {
+                pauseMenu.GoToAboveMenu();
+
+                if (success)
+                {
+                    setStringFunc(menuInputHandler.Text);
+                }
+                
+                menuInputHandler.Text = getStringFunc();
+            };
+
+            PauseMenu.SubMenu subMenu = new()
+            {
+                isBottomMenu = true,
+                depth = 1
+            };
+            subMenu.items.Add(subMenuItem);
+
+            pauseMenu.menus.Add(subMenu);
+
+            PauseMenu.MenuItem menuItem = CreateSubMenuItem(text, subMenu);
+            menuItem.func = DelegateSupport.ConvertDelegate<PauseMenu.menuFunctionDelegate>(new System.Action(() =>
+            {
+                pauseMenu.GoToBelowMenu();
+                textObject.alpha = 1.0F;
+                menuInputHandler.ActivateInputField();
+            }));
+
+            return menuItem;
+        }
+
+        public static void Postfix(PauseMenu __instance)
+        {
+            randomizerMenu = new();
+
+            pauseMenu = __instance;
+            pauseMenu.menus.Add(randomizerMenu);
+
+            __instance.mainMenu.items.Insert(3, CreateSubMenuItem("randomizer", randomizerMenu));
+
+            randomizerMenu.depth = 1;
+            randomizerMenu.items.Add(CreateReturnMenuItem());
+#if DEBUG
+            randomizerMenu.items.Add(CreateActionMenuItem("DEBUG: re-randomize", new System.Action(() =>
+            {
+                Randomizer.Instance?.GeneratePuzzles();
+                pauseMenu.ResumeGame();
+            })));
+#endif
+
+            _seedMenuItem = CreateTextEntryMenuItem(
+                "seed number",
+                () => _setSeedValue.ToString().PadLeft(7, '0'),
+                (string value) =>
+                {
+                    if (!int.TryParse(value, out _setSeedValue) || _setSeedValue < 1 || _setSeedValue >= 10000000)
+                    {
+                        _setSeedValue = 1;
+                    }
+                });
+
+            randomizerMenu.items.Add(CreateChoiceMenuItem(
+                "seed type",
+                new() { "random seed", "set seed" },
+                () => _setSeed ? 1 : 0,
+                (int choice) => SetSeed = (choice == 1)));
+            randomizerMenu.items.Add(_seedMenuItem);
+
+            SetSeed = SetSeed;
+        }
+    }
+}
---
+++ TextEntry.cs
@@ -0,0 +1,166 @@
+﻿using Il2Cpp;
+using Il2CppTMPro;
+using MelonLoader;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+namespace TaijiRandomizer
+{
+    [RegisterTypeInIl2Cpp]
+    class TextEntry : MonoBehaviour
+    {
+        public delegate void InputFinishedHandler(bool success);
+
+        private TextMeshPro? _inputField = null;
+        private GameManager? _gameManager = null;
+
+        private InputFinishedHandler? _inputFinishedHandler = null;
+        private PauseMenu.MenuItem? _menuItem = null;
+
+        private bool _inputActivated = false;
+        private bool _firstFrame = false;
+        private bool _shouldDeactivate = false;
+
+        private string _text = "";
+        private int _position = 0;
+
+        public string Text
+        {
+            get { return _text; }
+
+            set
+            {
+                if (!_text.Equals(value))
+                {
+                    _position = value.Length;
+
+                    SetText(value);
+                }
+            }
+        }
+
+        private void SetText(string value)
+        {
+            Randomizer.Instance?.LoggerInstance.Msg($"Text is changed from \"{_text}\" to \"{value}\"");
+
+            _text = value;
+
+            if (_inputField != null)
+            {
+                _inputField.text = _text;
+            }
+        }
+
+        public InputFinishedHandler OnInputFinished
+        {
+            set { _inputFinishedHandler = value; }
+        }
+
+        public PauseMenu.MenuItem MenuItem
+        {
+            set { _menuItem = value; }
+        }
+
+        public TextEntry(System.IntPtr ptr) : base(ptr) { }
+
+        void Start()
+        {
+            _inputField = gameObject.GetComponent<TextMeshPro>();
+            _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+            _inputField.text = _text;
+            _position = _text.Length;
+
+            Keyboard.current.add_onTextInput(new System.Action<char>(ProcessInput));
+        }
+
+        public void ActivateInputField()
+        {
+            _inputActivated = true;
+            _firstFrame = true;
+        }
+
+        public void DeactivateInputField()
+        {
+            _inputActivated = false;
+
+            if (_gameManager != null)
+            {
+                _gameManager.enabled = true;
+            }
+        }
+
+        void ProcessInput(char ch)
+        {
+            if (_inputActivated && !_firstFrame)
+            {
+                if (ch == '\b')
+                {
+                    if (_position > 0)
+                    {
+                        _position--;
+                        SetText(_text.Remove(_position, 1));
+                    }
+                }
+                else if (ch == '\n' || ch == '\r')
+                {
+                    _shouldDeactivate = true;
+                }
+                else if (!Char.IsControl(ch))
+                {
+                    SetText(_text.Insert(_position, ch.ToString()));
+                    _position++;
+                }
+            }
+        }
+
+        void Update()
+        {
+            if (_menuItem != null)
+            {
+                _menuItem.textWidth = 0.3F;
+            }
+
+            if (_firstFrame)
+            {
+                _firstFrame = false;
+
+                if (_gameManager != null)
+                {
+                    _gameManager.enabled = false;
+                }
+
+                return;
+            }
+
+            if (_shouldDeactivate)
+            {
+                DeactivateInputField();
+
+                if (_inputFinishedHandler != null)
+                {
+                    _inputFinishedHandler(true);
+                }
+
+                _shouldDeactivate = false;
+
+                return;
+            }
+
+            if (_inputActivated)
+            {
+                if (Keyboard.current.escapeKey.wasPressedThisFrame)
+                {
+                    DeactivateInputField();
+
+                    if (_inputFinishedHandler != null)
+                    {
+                        _inputFinishedHandler(false);
+                    }
+
+                    return;
+                }
+            }
+        }
+    }
+}
